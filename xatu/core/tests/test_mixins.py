fd18d1ef567589fa6d314d721f7a9926c787a768
alvarogf97 <ankora45@gmail.com>
2021-06-18 12:25:23 +0200

    init

--- xatu/core/tests/test_mixins.py
+++ xatu/core/tests/test_mixins.py
@@ -1,16 +1,42 @@
-from unittest import TestCase
+from unittest import TestCase, mock
+
+from xatu.connections.arango import db
+from xatu.core import mixins
 
 
-class WithCollectionBuilderTest(TestCase):
+class WithArangoCollectionBuilderTest(TestCase):
 
     def test_build(self):
-        pass
+        with mock.patch.object(
+            db, 'has_collection', return_value=False
+        ), \
+            mock.patch.object(
+            db, 'create_collection',
+        ) as mock_create_collection:
+            mixins.WithArangoCollectionBuilder.build()
+            mock_create_collection.assert_called_once()
 
     def test_drop(self):
-        pass
+        with mock.patch.object(
+            db, 'has_collection', return_value=True
+        ), \
+            mock.patch.object(
+            db, 'drop_collection',
+        ) as mock_drop_collection:
+            mixins.WithArangoCollectionBuilder.drop()
+            mock_drop_collection.assert_called_once()
 
     def test_query(self):
-        pass
+        with mock.patch.object(
+            db, 'query', return_value=True
+        ) as mock_query:
+            mixins.WithArangoCollectionBuilder.query()
+            mock_query.assert_called_once()
 
     def test_save(self):
-        pass
+        mixin = mixins.WithArangoCollectionBuilder()
+        with mock.patch.object(
+            db, 'add', return_value=True
+        ) as mock_add:
+            mixin.save()
+            mock_add.assert_called_once()
